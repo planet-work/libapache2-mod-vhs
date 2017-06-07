268367b6ebe87ce4b6772ae69a311279f0de7169
Frédéric VANNIÈRE <f.vanniere@planet-work.com>
2017-06-07 13:56:43 +0200

    Functional test with rewriting

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -30,6 +30,26 @@ mysqladmin create wordpress
 /usr/local/bin/wp --allow-root config create --dbname="wordpress" --dbuser="root" 
 /usr/local/bin/wp --allow-root core install --url=http://www.website.com/ --title="Test" --admin_user="admin" --admin_password="admincipwd" --admin_email="xxx@xx.com" --skip-email
 
+cat >/home/testuser/public_html/www/.htaccess << EOF
+# BEGIN WordPress
+<IfModule mod_rewrite.c>
+RewriteEngine On
+RewriteBase /
+RewriteRule ^index\.php$ - [L]
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule . /index.php [L]
+</IfModule>
+# END WordPress
+EOF
+
 curl -H 'Host: www.website.com'  http://127.0.0.1/
+POSTURL=$(/usr/local/bin/wp post list --field=url)
+echo $POSTURL
+echo ${POSTURL/www.website.com/127.0.0.1}
+echo "************************************************"
+curl -H 'Host: www.website.com'  $POSTURL | grep bonjour
+echo "************************************************"
+curl -H 'Host: www.website.com'  ${POSTURL/www.website.com/127.0.0.1} | grep bonjour
 
 exit 0
